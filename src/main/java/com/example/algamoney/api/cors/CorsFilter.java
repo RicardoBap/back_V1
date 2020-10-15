9ee301f75998cac7da8ebe000326ac466d94882a
Ricardo <ric_bap@hotmail.com>
2020-10-15 20:07:15 -0300

    Correção CORS

--- src/main/java/com/example/algamoney/api/cors/CorsFilter.java
+++ src/main/java/com/example/algamoney/api/cors/CorsFilter.java
@@ -11,16 +11,19 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.example.algamoney.api.config.property.SalvavidasApiProperty;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 	
-	//@Autowired
-	//private SalvavidasApiProperty salvavidasApiProperty;
+	@Autowired
+	private SalvavidasApiProperty salvavidasApiProperty;
 	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
@@ -32,12 +35,12 @@ public class CorsFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) resp;
 		
-		response.setHeader("Access-Control-Allow-Origin", originPermitida);
-		//response.setHeader("Access-Control-Allow-Origin", salvavidasApiProperty.getOriginPermitida());
+		//response.setHeader("Access-Control-Allow-Origin", originPermitida);
+		response.setHeader("Access-Control-Allow-Origin", salvavidasApiProperty.getOriginPermitida());
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		
-		if ("OPTIONS".contentEquals(request.getMethod()) && originPermitida.contentEquals(request.getHeader("Origin")) ) {
-		//if ("OPTIONS".contentEquals(request.getMethod()) && salvavidasApiProperty.getOriginPermitida().contentEquals(request.getHeader("Origin")) ) {	
+		//if ("OPTIONS".contentEquals(request.getMethod()) && originPermitida.contentEquals(request.getHeader("Origin")) ) {
+		if ("OPTIONS".equals(request.getMethod()) && salvavidasApiProperty.getOriginPermitida().equals(request.getHeader("Origin")) ) {	
 			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
 			response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
 			response.setHeader("Access-Control-Max-Age", "3600");
